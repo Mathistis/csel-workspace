6da69ee78974b014da1781507f09424693559a14
Raemy Mathis <mathis.raemy@gmail.com>
2022-02-28 16:01:33 +0000

    ajout de MakeFile et modification du rapport

---
+++ Makefile
@@ -0,0 +1,13 @@
+# Makefile toolchain part
+WC=$(wildcard *.c)
+TOOLCHAIN_PATH=/buildroot/output/host/usr/bin/
+TOOLCHAIN=$(TOOLCHAIN_PATH)aarch64-linux-
+APPNAME=app
+# Makefile common part
+CC=$(TOOLCHAIN)gcc
+LD=$(TOOLCHAIN)gcc
+AR=$(TOOLCHAIN)ar
+CFLAGS+=-Wall -Wextra -g -c -mcpu=cortex-a53 -O0 -MD -std=gnu11
+OUTPUT=-o $(APPNAME)
+all:
+	$(CC)	$(CFLAGS)	$(OUTPUT)	$(wildcard *.c)
---
+++ app.d
@@ -0,0 +1,26 @@
+app: test.c \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/stdc-predef.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/stdio.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/libc-header-start.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/features.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/features-time64.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/wordsize.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/timesize.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/sys/cdefs.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/long-double.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/gnu/stubs.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/gnu/stubs-lp64.h \
+ /buildroot/output/host/lib/gcc/aarch64-buildroot-linux-gnu/10.3.0/include/stddef.h \
+ /buildroot/output/host/lib/gcc/aarch64-buildroot-linux-gnu/10.3.0/include/stdarg.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/typesizes.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/time64.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/__fpos_t.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/__mbstate_t.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/__fpos64_t.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/__FILE.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/FILE.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/types/struct_FILE.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/stdio_lim.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/floatn.h \
+ /buildroot/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/bits/floatn-common.h
